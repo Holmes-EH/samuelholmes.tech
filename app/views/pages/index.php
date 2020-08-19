81bf98af81471f85dac59f3eabdb1932b3f12736
Holmes-EH <holmes.samuel@yahoo.fr>
2020-08-19 12:54:13 +0200

    setup to have something up there !!

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,5 +1,26 @@
 <?php require_once '../app/views/inc/header.php'; ?>
 
-<h1><?php echo $data['title']?></h1>
+<div class="main">
+    <div class="mainLogo">
+        <img id="imgCont" src="<?php echo URLROOT; ?>/images/SHLogo-02.png" alt="Samuel Holmes Tech Logo">
+    </div>
+    <div class="mainTagCont" id="txtCont">
+        <div class="mainTagline">
+            <p class="tagline">Techniquement ?<br><br>Tout est possible...</p>
+            <p class="desc">Solutions web sur mesure</p>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function(){
+        $("#txtCont").height($("#imgCont").height());
+        $(window).resize(function() {
+            $("#txtCont").height($("#imgCont").height());
+        });
+    });
+        
+    
+</script>
 
 <?php require_once '../app/views/inc/footer.php'; ?>
