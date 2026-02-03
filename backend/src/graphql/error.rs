06faafbd1334af00aedac02e703aedb62d8cb3c0
Holmes-EH <s.holmes.eh@gmail.com>
2026-02-03 17:24:26 +0100

    feat: add custom graphql error

---
+++ backend/src/graphql/error.rs
@@ -0,0 +1,38 @@
+use async_graphql::ErrorExtensions;
+
+#[derive(Debug)]
+pub enum AppError {
+    NotFound(String),
+    InvalidInput(String),
+    DatabaseError(String),
+}
+
+impl std::fmt::Display for AppError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AppError::NotFound(msg) => write!(f, "{}", msg),
+            AppError::InvalidInput(msg) => write!(f, "{}", msg),
+            AppError::DatabaseError(msg) => write!(f, "{}", msg),
+        }
+    }
+}
+
+impl std::error::Error for AppError {}
+
+impl ErrorExtensions for AppError {
+    fn extend(&self) -> async_graphql::Error {
+        let mut err = async_graphql::Error::new(self.to_string());
+
+        match self {
+            AppError::NotFound(_) => err = err.extend_with(|_, e| e.set("code", "NOT_FOUND")),
+            AppError::InvalidInput(_) => {
+                err = err.extend_with(|_, e| e.set("code", "INVALID_INPUT"))
+            }
+            AppError::DatabaseError(_) => {
+                err = err.extend_with(|_, e| e.set("code", "DATABASE_ERROR"))
+            }
+        }
+
+        err
+    }
+}
--- backend/src/graphql/mod.rs
+++ backend/src/graphql/mod.rs
@@ -1,3 +1,4 @@
+mod error;
 mod mutations;
 mod queries;
 pub mod schema;
